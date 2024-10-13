a2f9c14a3de0c1a881011b6cf15d70e93c6d2a81
QAFed <143259597+QAFed@users.noreply.github.com>
2024-10-13 15:44:07 +0300

    order test 2-3 done

--- tests/test_order_create.py
+++ tests/test_order_create.py
@@ -1,7 +1,10 @@
+import pytest
+
 from endpoints.order_create import OrderCreate
 
 
 class TestOrderCreate:
+
     def test_order_create_if_user_authorized(self, add_and_delete_gen_user):
         gen_user_data = add_and_delete_gen_user
         ingred_id_payload = {
@@ -9,9 +12,25 @@ class TestOrderCreate:
         }
         order_create = OrderCreate(ingred_id_payload, gen_user_data.access_token)
         order_create.request()
-        print("ответ", order_create.response.json())
         order_create.check_status_code(200)
         order_create.check_id_in_order_list(ingred_id_payload["ingredients"])
         order_create.check_order_number_exist()
 
+    def test_order_not_create_if_user_authorized_and_incorrect_id_hesh(self, add_and_delete_gen_user):
+        gen_user_data = add_and_delete_gen_user
+        gen_payload = {
+            "ingredients": ["60d3b41abdacab0026a733c6", "609646e4dc916e00276b2870"]
+        }
+        order_create = OrderCreate(gen_payload, gen_user_data.access_token)
+        order_create.request()
+        order_create.check_status_code(400)
+        order_create.check_text_message("One or more ids provided are incorrect")
+
+    def test_order_not_create_if_user_authorized_and_ids_apsent(self, add_and_delete_gen_user):
+        gen_user_data = add_and_delete_gen_user
+        gen_payload = {}
+        order_create = OrderCreate(gen_payload, gen_user_data.access_token)
+        order_create.request()
+        order_create.check_status_code(400)
+        order_create.check_text_message("Ingredient ids must be provided")
 
